87b781dc4c035bf387168f0b8daf2903a86d15d0
Tharangani <tharangijayamuthu@gmail.com>
2026-07-19 20:54:12 +0530

    create view all task logic and route

--- backend/controllers/task.controller.js
+++ backend/controllers/task.controller.js
@@ -1,5 +1,6 @@
 import { Task } from "../models/task.model.js";
 
+//create new task
 export const addTask = async (req, res, next) => {
     try {
         const { title, description, status, priority, dueDate } = req.body;
@@ -27,3 +28,41 @@ export const addTask = async (req, res, next) => {
 
     }
 };
+
+//create new task
+export const getAllTask = async (req, res, next) => {
+    try {
+        const tasks = await Task.find({
+            owner: req.user._id
+        }).sort({
+            dueDate:1
+        });
+
+        const priorityOrder ={
+            high: 1,
+            medium: 2,
+            low: 3,
+        }
+
+        //sort tasks by piority first and then due date
+        tasks.sort((a,b) =>{
+            if(priorityOrder[a.priority] !== priorityOrder[b.priority]){
+                return priorityOrder[a.priority] - priorityOrder[b.priority];
+            }
+            return new Date(a.dueDate) - new Date(b.dueDate)
+        });
+
+        res.status(200).json({
+            totalTasks: tasks.length,
+            tasks
+
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+
+    }
+};
+
