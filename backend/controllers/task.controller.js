6ed1e88212bf6f20c2f748be541277e3e47a007e
Tharangani <tharangijayamuthu@gmail.com>
2026-07-20 12:45:31 +0530

    create update task logic and route

--- backend/controllers/task.controller.js
+++ backend/controllers/task.controller.js
@@ -202,3 +202,39 @@ export const updateTaskStatus = async (req, res, next) => {
 
     }
 };
+
+export const updateTask = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { title, description, priority, status, dueDate } = req.body;
+
+        //check validation of object id
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({
+                message: "Invalid task ID"
+            });
+        }
+
+        //find task by id and update 
+        const task = await Task.findByIdAndUpdate(id, { title, description, priority, status, dueDate }, {
+            new: true,
+            runValidators: true
+        });
+        if (!task) {
+            return res.status(404).json({
+                message: "Task not found"
+            });
+        }
+
+        res.status(200).json({
+            message: "Task  updated successfully",
+            task
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+
+    }
+};
--- backend/routes/task.route.js
+++ backend/routes/task.route.js
@@ -2,7 +2,7 @@ import express from "express";
 import { validate } from "../middlewares/validate.middleware.js";
 import { addTaskValidateSchema } from "../validators/task.validator.js";
 import { protect } from "../middlewares/auth.middleware.js";
-import { addTask, deleteTask, getAllTask, getSingleTask, updateTaskStatus } from "../controllers/task.controller.js";
+import { addTask, deleteTask, getAllTask, getSingleTask, updateTask, updateTaskStatus } from "../controllers/task.controller.js";
 
 export const taskRoute = express.Router();
 
@@ -20,3 +20,6 @@ taskRoute.delete('/tasks/:id',protect, deleteTask);
 
 //update  task status
 taskRoute.patch('/tasks/:id/status',protect, updateTaskStatus);
+
+//update  task
+taskRoute.put('/tasks/:id',protect, updateTask);
